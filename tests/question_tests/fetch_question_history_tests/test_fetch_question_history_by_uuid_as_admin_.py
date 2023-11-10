0415e218e45b517a521c6a4fde91797a525ccc3e
Khyn Antoque <khynantoque@gmail.com>
2023-11-11 00:45:28 +0800

    finished tests

--- tests/question_tests/fetch_question_history_tests/test_fetch_question_history_by_uuid_as_admin_.py
+++ tests/question_tests/fetch_question_history_tests/test_fetch_question_history_by_uuid_as_admin_.py
@@ -1,7 +1,10 @@
+from bson import ObjectId
 from pytest import fixture
 import pdb, requests
 import os, sys, json
 from assertpy import assert_that
+
+from tests.lib.mw_db import get_db
 
 CURRENT_DIR = os.getcwd()
 PARENT_DIR = os.path.dirname(CURRENT_DIR)
@@ -12,7 +15,6 @@ import logging as logger, pytest
 import lib.common as common
 import lib.generate_token as generate_token
 from lib.requester import Requester
-from lib.mw_sql import execute_query
 
 
 
@@ -24,29 +26,27 @@ def get_admin_token():
 
 
 @pytest.mark.tc_001
-def test_fetch_staar_question_history_by_uuid(get_admin_token):
-    req: Requester = Requester()      
-    sql_staar_returned: list = execute_query(
-        f"SELECT * FROM mathworld.question WHERE question_type = 'STAAR' LIMIT 1")        
+def test_fetch_staar_question_history_by_id(get_admin_token):
+    req: Requester = Requester()    
+    sql_staar_returned: dict = get_db().question_collection.find_one({'question_type': 'STAAR'})    
     headers: dict = req.create_basic_headers(token=get_admin_token)    
-    sql_uuid: str = sql_staar_returned[0][0]
-    url: str = f"{req.base_url}/question/fetch/history/{sql_uuid}"      
+    sql_id: str = sql_staar_returned['_id']
+    url: str = f"{req.base_url}/v1/questions/{sql_id}"      
     response = requests.request("GET", url, headers=headers)
     assert response.status_code == 200
     questions: dict = json.loads(response.text)
-    assert_that(questions['history'][0]['question_uuid']).is_equal_to(sql_uuid)
-    assert_that(len(questions['history'])).is_greater_than(0)
-    pass
+    assert_that(ObjectId(questions['question']['id'])).is_equal_to(ObjectId(sql_id))
+    # assert_that(len(questions['history'])).is_greater_than(0)
 
 @pytest.mark.tc_002
 def test_fetch_staar_question_history_by_invalid_uuid(get_admin_token):
     req: Requester = Requester()      
-    sql_staar_returned: list = execute_query(
-        f"SELECT * FROM mathworld.question WHERE question_type = 'STAAR' LIMIT 1")        
+    sql_staar_returned: dict = get_db().question_collection.find_one({'question_type': 'STAAR'})
+            
     headers: dict = req.create_basic_headers(token=get_admin_token)    
-    sql_uuid: str = sql_staar_returned[0][0] + "123"
-    url: str = f"{req.base_url}/question/fetch/history/{sql_uuid}"          
+    sql_uuid: str = str(sql_staar_returned['_id']) + "123"
+    url: str = f"{req.base_url}/v1/questions/{sql_uuid}"          
     response = requests.request("GET", url, headers=headers)
     assert response.status_code == 400
     json_response: dict = json.loads(response.text)    
-    assert_that(json_response['detail']).is_equal_to("Invalid uuid")
+    assert_that(json_response['detail']).is_equal_to("Question not found")
